a9cf93fdd572f5d8831402426bbdbe7c21130d9e
James Bennett <jimmyb1014@hotmail.com>
2023-01-27 07:25:50 -0600

    add score btn functions

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 let homeScoreEl = document.getElementById("home-score-el")
 let awayScoreEl = document.getElementById("away-score-el")
 let homeCount = 0
+let awayCount = 0
+
+function homePlusOne() {
+    homeCount += 1
+    homeScoreEl.textContent = homeCount
+}
+
+function homePlusTwo() {
+    homeCount += 2
+    homeScoreEl.textContent = homeCount
+}
+
+function homePlusThree() {
+    homeCount += 3
+    homeScoreEl.textContent = homeCount
+}
+
+function awayPlusOne() {
+    awayCount += 1
+    awayScoreEl.textContent = awayCount
+}
+
+function awayPlusTwo() {
+    awayCount += 2
+    awayScoreEl.textContent = awayCount
+}
+
+function awayPlusThree() {
+    awayCount += 3
+    awayScoreEl.textContent = awayCount
+}
 
