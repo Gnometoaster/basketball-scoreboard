6ac380d2efae07a4a4ea7aa004748d868ac99303
James Bennett <jimmyb1014@hotmail.com>
2023-01-27 06:37:37 -0600

    Fine tuned styling

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 let homeScoreEl = document.getElementById("home-score-el")
-let awayScoreElement = document.getElementById("away-score-el")
+let awayScoreEl = document.getElementById("away-score-el")
+let homeCount = 0
+
