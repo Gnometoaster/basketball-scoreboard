736cc9e3ebc7be2d0c09f96e6defcaaa3e4a9047
James Bennett <jimmyb1014@hotmail.com>
2023-01-27 21:19:50 -0600

    American football style and functions. alert if bears win

--- index.js
+++ index.js
@@ -3,33 +3,64 @@ let awayScoreEl = document.getElementById("away-score-el")
 let homeCount = 0
 let awayCount = 0
 
-function homePlusOne() {
+function homeTouchdown() {
+    homeCount += 6
+    homeScoreEl.textContent = homeCount
+}
+
+function homeExtraOne() {
     homeCount += 1
     homeScoreEl.textContent = homeCount
 }
 
-function homePlusTwo() {
+function homeExtraTwo() {
     homeCount += 2
     homeScoreEl.textContent = homeCount
 }
 
-function homePlusThree() {
+function homeFieldGoal() {
     homeCount += 3
     homeScoreEl.textContent = homeCount
 }
 
-function awayPlusOne() {
+function homeSafety() {
+    homeCount += 2
+    homeScoreEl.textContent = homeCount
+}
+
+function awayTouchdown() {
+    awayCount += 6
+    awayScoreEl.textContent = awayCount
+}
+
+function awayExtraOne() {
     awayCount += 1
     awayScoreEl.textContent = awayCount
 }
 
-function awayPlusTwo() {
+function awayExtraTwo() {
     awayCount += 2
     awayScoreEl.textContent = awayCount
 }
 
-function awayPlusThree() {
+function awayFieldGoal() {
     awayCount += 3
     awayScoreEl.textContent = awayCount
 }
 
+function awaySafety() {
+    awayCount += 2
+    awayScoreEl.textContent = awayCount
+}
+
+function newGame() {
+    if(homeCount > awayCount) {
+        alert("You know the Bears arent allowed to win, try again!" )
+    }
+    else {
+    homeScoreEl.textContent = 0
+    awayScoreEl.textContent = 0
+    homeCount = 0
+    awayCount = 0
+    }
+}
